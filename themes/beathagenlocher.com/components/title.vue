408d95f9b08886a26f493dd936768c9f8e818e3f
Beat Hagenlocher <bah@posteo.de>
2025-07-22 13:11:14 +0200

    Update title

--- themes/beathagenlocher.com/components/title.vue
+++ themes/beathagenlocher.com/components/title.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-5xl leading-20 font-light uppercase">
+  <h1 class="max-w-xl mx-auto my-5 text-5xl leading-15 font-light uppercase">
     <slot />
   </h1>
 </template>
